f933cd499b59dc6cb7526f916113482ebb26347c
Erick Joel Hidalgo Valdivia <erick.hidalgo@e-capture.co>
2024-02-22 19:17:42 -0500

    first commit

---
+++ src/app/ui/block-page/block-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-block-page',
+  templateUrl: './block-page.component.html',
+  styleUrls: ['./block-page.component.css']
+})
+export class BlockPageComponent {
+
+}
